046a6236f3a7a6ca3ae552a0656f7b50a7b63de7
Will <gogobebe2@gmail.com>
2015-11-21 19:46:00 +1300

    Update AntiSwearPluginMain.java

--- src/me/lightcab/AntiSwearPluginMain.java
+++ src/me/lightcab/AntiSwearPluginMain.java
@@ -3,11 +3,13 @@ package me.lightcab;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AntiSwearPluginMain extends JavaPlugin {
+    protected static AntiSwearPluginMain instance;
 
     @Override
     public void onEnable() {
+        instance = this;
         getLogger().info("The plugin has been enabled!");
-        getServer().getPluginManager().registerEvents(new AntiSwearPluginListener(this), this);
+        getServer().getPluginManager().registerEvents(new AntiSwearPluginListener(), this);
         saveDefaultConfig();
     }
 
