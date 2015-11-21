bfbf3440805c890219d6967e9fafe1f3fd26975f
Lightcab <rijalaashish0@gmail.com>
2015-11-21 09:20:24 +0545

    Some bugs has been fixed.

--- src/me/lightcab/AntiSwearPluginListener.java
+++ src/me/lightcab/AntiSwearPluginListener.java
@@ -8,6 +8,7 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class AntiSwearPluginListener implements Listener {
 
+
     AntiSwearPluginMain plugin;
 
     public AntiSwearPluginListener(AntiSwearPluginMain instance) {
@@ -17,18 +18,28 @@ public class AntiSwearPluginListener implements Listener {
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent e) {
         Player p = e.getPlayer();
+        boolean playerhasSweared = false;
         String message = e.getMessage().toLowerCase();
         for (String x : plugin.getConfig().getStringList("bannedWords")) {
             if (message.contains(x.toLowerCase())) {
-                if (plugin.getConfig().getBoolean("warnPlayer")) {
-                    p.sendMessage(ChatColor.GREEN + "AntiSwear> " + ChatColor.WHITE + plugin.getConfig().getString("replaceMessage"));
-                }
-                e.setMessage(plugin.getConfig().getString("replaceMessage"));
-                if (plugin.getConfig().getBoolean("setFire")) {
-                    p.setFireTicks(plugin.getConfig().getInt("fireSecond") * 20);
-                }
+                playerhasSweared = true;
             }
 
         }
+        if (playerhasSweared) {
+            e.setMessage(plugin.getConfig().getString("replaceMessage"));
+            if (plugin.getConfig().getBoolean("warnPlayer")) {
+                p.sendMessage(ChatColor.GREEN + "AntiSwear> " + ChatColor.WHITE + plugin.getConfig().getString("warnMessage"));
+            }
+            if (plugin.getConfig().getBoolean("setFire")) {
+                p.setFireTicks(plugin.getConfig().getInt("fireSecond") * 20);
+            }
+            if (plugin.getConfig().getBoolean("strikeLightning")) {
+                for (int loopNumber = 0; loopNumber < plugin.getConfig().getInt("lightningNumber"); loopNumber++) {
+                    p.getLocation().getWorld().strikeLightning(p.getLocation());
+                }
+            }
+        }
+
     }
 }
